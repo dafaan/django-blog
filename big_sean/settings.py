3df9f97374ee1f1f5d0316345fb0ad0be70d9352
dafaan <dafaanmoses753@gmail.com>
2022-06-14 03:25:41 +0100

    debug changed to false

--- big_sean/settings.py
+++ big_sean/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'r6kquha+r0%3tu#jvj6zrytyw!$0c+@!%5-y)+28c)g3sj@osq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
